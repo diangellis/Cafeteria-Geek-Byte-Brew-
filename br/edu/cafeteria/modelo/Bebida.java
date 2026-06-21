ee7aca7caf9132498dfc0bf9d7bc6ef397abe8e5
Warlley <wmedeiros.d@gmail.com>
2026-06-20 23:08:17 -0300

    versao 1 da Comida e da Bebida, somente com os atributos privados e nada mais

---
+++ br/edu/cafeteria/modelo/Bebida.java
@@ -0,0 +1,6 @@
+package edu.cafeteria.modelo;
+
+public class Bebida extends Product{
+    private double qnt_cafeina;
+    private Tamanho tamanho;
+}
---
+++ br/edu/cafeteria/modelo/Comida.java
@@ -0,0 +1,7 @@
+package edu.cafeteria.modelo;
+
+public class Comida extends Product{
+    private int tempo_Preparo;
+    private boolean isVegano;
+    private boolean contem_glutem;
+}
