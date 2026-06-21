462f9cc8b31ee01005a02dbfcfb5f29873464040
WarlleyMedeiros <warlleydiasxz@gmail.com>
2026-06-21 09:52:49 -0300

    adicionado get set e construtores

--- br/edu/cafeteria/modelo/Bebida.java
+++ br/edu/cafeteria/modelo/Bebida.java
@@ -1,6 +1,32 @@
 package edu.cafeteria.modelo;
 
+import edu.cafeteria.modelo.Tamanho;
+
 public class Bebida extends Product{
     private double qnt_cafeina;
     private Tamanho tamanho;
+
+    public Bebida() {
+    }
+
+    public Bebida(double qnt_cafeina, Tamanho tamanho) {
+        this.qnt_cafeina = qnt_cafeina;
+        this.tamanho = tamanho;
+    }
+
+    public double getQnt_cafeina() {
+        return qnt_cafeina;
+    }
+
+    public void setQnt_cafeina(double qnt_cafeina) {
+        this.qnt_cafeina = qnt_cafeina;
+    }
+
+    public Tamanho getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(Tamanho tamanho) {
+        this.tamanho = tamanho;
+    }
 }
--- br/edu/cafeteria/modelo/Comida.java
+++ br/edu/cafeteria/modelo/Comida.java
@@ -4,4 +4,37 @@ public class Comida extends Product{
     private int tempo_Preparo;
     private boolean isVegano;
     private boolean contem_glutem;
+
+    public Comida() {
+    }
+
+    public Comida(int tempo_Preparo, boolean contem_glutem, boolean isVegano) {
+        this.tempo_Preparo = tempo_Preparo;
+        this.contem_glutem = contem_glutem;
+        this.isVegano = isVegano;
+    }
+
+    public int getTempo_Preparo() {
+        return tempo_Preparo;
+    }
+
+    public void setTempo_Preparo(int tempo_Preparo) {
+        this.tempo_Preparo = tempo_Preparo;
+    }
+
+    public boolean isVegano() {
+        return isVegano;
+    }
+
+    public void setVegano(boolean vegano) {
+        isVegano = vegano;
+    }
+
+    public boolean isContem_glutem() {
+        return contem_glutem;
+    }
+
+    public void setContem_glutem(boolean contem_glutem) {
+        this.contem_glutem = contem_glutem;
+    }
 }
