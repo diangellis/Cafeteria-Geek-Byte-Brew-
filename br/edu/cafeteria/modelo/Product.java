6049188b54efe62e49916c59ee807426bc467a9f
WarlleyMedeiros <warlleydiasxz@gmail.com>
2026-06-24 12:23:07 -0300

    adicionando ToString do Produto

--- br/edu/cafeteria/modelo/Product.java
+++ br/edu/cafeteria/modelo/Product.java
@@ -1,6 +1,6 @@
 package edu.cafeteria.modelo;
 
-abstract class Product implements Entidade{
+public abstract class Product implements Entidade{
     private String nome;
     private String codigo;
     private double preco_Base;
@@ -11,4 +11,20 @@ abstract class Product implements Entidade{
         return this.codigo;
     }
 
+    public Product(String nome, String codigo, double preco_Base, int qntd_estocada) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.preco_Base = preco_Base;
+        this.qntd_estocada = qntd_estocada;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("nome: ").append(this.nome).append("\n");
+        s.append("codigo: ").append(this.codigo).append("\n");
+        s.append("preço base: ").append(this.preco_Base).append("\n");
+        s.append("Estoque: ").append(this.qntd_estocada).append("\n");
+        return s.toString();
+    }
 }
