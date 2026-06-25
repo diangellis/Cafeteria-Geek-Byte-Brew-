fa8416751d1fcc32e4ceda90fb744c7d02ccd769
WarlleyMedeiros <warlleydiasxz@gmail.com>
2026-06-25 08:39:44 -0300

    edição na forma como a exceção funciona

--- br/edu/cafeteria/excecao/EstoqueInsuficienteException.java
+++ br/edu/cafeteria/excecao/EstoqueInsuficienteException.java
@@ -1,7 +1,34 @@
 package edu.cafeteria.excecao;
 
 public class EstoqueInsuficienteException extends RuntimeException {
-    public EstoqueInsuficienteException(String message) {
-        super(message);
+
+    private final int quantidadeSolicitada;
+    private final int quantidadeDisponivel;
+
+
+    public EstoqueInsuficienteException(int quantidadeSolicitada, int quantidadeDisponivel) {
+
+        super(String.format(
+                "Estoque insuficiente. Solicitado: %d | Disponível: %d | Faltam: %d unidades.",
+                quantidadeSolicitada,
+                quantidadeDisponivel,
+                quantidadeSolicitada - quantidadeDisponivel
+        ));
+
+        this.quantidadeSolicitada = quantidadeSolicitada;
+        this.quantidadeDisponivel = quantidadeDisponivel;
+    }
+
+
+    public int getQuantidadeSolicitada() {
+        return quantidadeSolicitada;
+    }
+
+    public int getQuantidadeDisponivel() {
+        return quantidadeDisponivel;
+    }
+
+    public int getQuantidadeFaltante() {
+        return quantidadeSolicitada - quantidadeDisponivel;
     }
 }
