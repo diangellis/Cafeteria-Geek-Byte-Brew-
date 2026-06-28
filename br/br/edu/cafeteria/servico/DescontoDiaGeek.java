15dff2b643f749c4cbac6f206cfce36b00c4fe7c
Yasmin de Loiola Araujo <araujoy683@gmail.com>
2026-06-28 16:19:00 -0300

    Reapply "Merge branch 'main' of https://github.com/diangellis/Cafeteria-Geek-Byte-Brew-"
    This reverts commit 0bd65ea499ae895a67c7cf54292c249a3588e05b.

--- br/br/edu/cafeteria/servico/DescontoDiaGeek.java
+++ br/br/edu/cafeteria/servico/DescontoDiaGeek.java
@@ -3,6 +3,8 @@ package br.edu.cafeteria.servico;
 import br.edu.cafeteria.modelo.Bebida;
 import br.edu.cafeteria.modelo.ItemPedido;
 import br.edu.cafeteria.modelo.Venda;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DescontoDiaGeek implements Promocional {
 
@@ -10,10 +12,16 @@ public class DescontoDiaGeek implements Promocional {
 
     @Override
     public void aplicarDesconto(Venda venda) {
-        for (ItemPedido item : venda.getItens()) {
+        // Criamos uma cópia local apenas para ler os itens com segurança
+        List<ItemPedido> copiaItens = new ArrayList<>(venda.getItens());
+
+        for (ItemPedido item : copiaItens) {
             if (item.getProduto() instanceof Bebida) {
-                double precoComDesconto = item.getProduto().getPreco_Base() * (1 - DESCONTO_BEBIDA);
-                item.setPrecoUnitarioAplicado(precoComDesconto);
+                double precoBase = item.getProduto().getPreco_Base();
+                double precoComDesconto = precoBase * (1 - DESCONTO_BEBIDA);
+
+                // Pedimos para a própria venda atualizar o valor do item no carrinho dela
+                venda.atualizarPrecoDoItem(item.getProduto(), precoComDesconto);
             }
         }
     }
